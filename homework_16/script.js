cbdc5fbb401f66bc5e7f261bc39d2c64b8dc1d7a
Anna Radchenko <annaradchenko137@gmail.com>
2023-09-23 00:49:54 +0300

    add homowork_16

---
+++ homework_16/script.js
@@ -0,0 +1,26 @@
+const btns = document.querySelectorAll(`[data-btn="action"]`);
+console.log(btns);
+
+const firstButton = document.querySelector(`#firstButton`);
+const secondButton = document.querySelector(`#secondButton`);
+const thirdButton = document.querySelector(`#thirdButton`);
+
+const firstFunc = (e) => {
+    console.log(`First func for ${e.target.innerHTML}`);
+    e.target.removeEventListener(`click`, firstFunc);
+    e.target.addEventListener(`click`, secondFunc);
+}
+
+const secondFunc = (e) => {
+    console.log(`Second func for ${e.target.innerHTML}`);
+    e.target.removeEventListener(`click`, secondFunc);
+    e.target.addEventListener(`click`, lastFunc);
+}
+
+const lastFunc = (e) => {
+    console.log(`The last func for ${e.target.innerHTML}`);
+}
+
+btns.forEach((element) => {
+    element.addEventListener(`click`, firstFunc); 
+});
